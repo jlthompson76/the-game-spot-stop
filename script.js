6fd2bd9f6f4c2d910cbb26d7808e1267624a1636
Jamie Thompson <jlthompson76@users.noreply.github.com>
2020-04-14 23:09:25 -0400

    Modify script.js and index.html files

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function addGame() {
 
     let title = prompt(`Please enter the title of a recent game you've played:`);
-    let score = Number(prompt(`On a scale of 0-5, how would you rate this game?`));
+    let score = Number(prompt(`On a scale of 0-5, how would you rate ${title}?`));
+    if (score > 5) {
+        score = Number(prompt(`Please rate ${title} on a scale of 0 to 5.`));
+    }
+    console.log(title, score);
 
     if (score > 3) {
         document.querySelector(".title").style.color = "green";
@@ -9,7 +13,11 @@ function addGame() {
         document.querySelector(".title").style.color = "red";
     }
 
-    document.querySelector(".title").textContent = title;
-    document.querySelector(".score").textContent = score;
+    let titleP = document.querySelector(".title");
+    console.log(titleP.textContent);
+    titleP.innerHTML += `<br>You recently played ${title}.`;
+    let scoreP = document.querySelector(".score");
+    console.log(scoreP.textContent);
+    scoreP.innerHTML += `<br>You gave ${title} a score of ${score} out of 5.`;
 
 }
